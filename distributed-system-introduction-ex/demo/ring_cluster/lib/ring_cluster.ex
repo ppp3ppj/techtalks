1dc0e660fa6f6643e4c239776f7bf3f5114d8325
ppp3ppj <ppp.3ppj@gmail.com>
2025-08-18 22:25:26 +0700

    add ring cluster demo

---
+++ distributed-system-introduction-ex/demo/ring_cluster/lib/ring_cluster.ex
@@ -0,0 +1,18 @@
+defmodule RingCluster do
+  @moduledoc """
+  Documentation for `RingCluster`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RingCluster.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ distributed-system-introduction-ex/demo/ring_cluster/mix.exs
@@ -0,0 +1,31 @@
+defmodule RingCluster.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ring_cluster,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {RingCluster.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:libcluster, "~> 3.5.0"},
+      {:delta_crdt, "~> 0.6.5"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
